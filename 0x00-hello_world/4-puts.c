8d9677bfc68b0b7b3d1b799d7c8a4032c7ac54e5
Ife <ife.ifeoluwa@gmail.com>
2022-12-12 23:28:04 -0800

    preprocessor export file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include <stdio.h> 
-  
- /** 
-  * main - prints exactly "Programming is like building a multilingual puzzle" on the screen
-  * Return: 0 if exited properly, non-zero otherwise 
-  */ 
- int main(void) 
- { 
-         puts("\"Programming is like building a multilingual puzzle"); 
-         return (0); 
- }
+#include <stdio.h>
+/**
+ * main - prints a phrase on the screen
+ * Return: 0 if exited properly
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
